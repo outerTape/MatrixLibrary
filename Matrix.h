fe459586a89922c480303b8a3fbf073d449d2b08
outerTape <42237490+outerTape@users.noreply.github.com>
2020-03-15 18:31:23 +0100

    Added constructor and fill fucntion

--- Matrix.h
+++ Matrix.h
@@ -1,10 +1,17 @@
 #include <vector>
 
 struct Matrix{
-    std::vector<float> values;
+    std::vector<float> matrix_values;
     int n_rows, n_cols;
 
     Matrix(int a, int b){
-
+        n_rows = a;
+        n_cols = b;
     }
+
+    void fill(float a){
+        matrix_values.assign(n_rows*n_cols, a);
+    }
+
+    
 };
