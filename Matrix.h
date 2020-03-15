272a5493cfc9d69b2ecfb3a9ebc3c77009bdf216
outerTape <42237490+outerTape@users.noreply.github.com>
2020-03-15 19:37:07 +0100

    Added + and () operators

--- Matrix.h
+++ Matrix.h
@@ -13,5 +13,17 @@ struct Matrix{
         matrix_values.assign(n_rows*n_cols, a);
     }
 
-    
+    float& operator()(int x, int y){
+        return matrix_values[x+n_rows*y];
+    }
+
+    Matrix operator+(Matrix& rhs){
+        Matrix sum(n_rows, n_cols);
+        for(int i = 0; i < n_rows; i++){
+            for(int j = 0; j < n_cols; j++){
+                sum(i,j) = (*this)(i,j) + rhs(i,j);
+            }
+        }
+        return sum;
+    }
 };
